d6a7fa5cd501361a4589815dab6ad23142e9ad10
EmilyPlatzer <emilyplatzer@gmail.com>
2015-09-01 13:12:20 -0600

    User can hipstafy their phrase

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var hipList = require('../lib/snippets.js')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.post('/', function(req, res, next) {
+  var hipstafied = '';
+  req.body.unhiptext.split(' ').forEach(function(unhipword) {
+    hipstafied += unhipword + ' ' + hipList[Math.round( Math.random() * hipList.length )] + ' '
+  })
+  res.render('index', {hipstafied: hipstafied});
 });
 
 module.exports = router;
